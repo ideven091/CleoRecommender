2fe94eeda387ffc980d228438093a2169fd50f2f
Admin <admin@Mac.local>
2022-09-10 22:18:47 +0530

    Seed

---
+++ src/main/java/com/cleo/CleoRecommender/CleoRecommenderApplication.java
@@ -0,0 +1,13 @@
+package com.cleo.CleoRecommender;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CleoRecommenderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CleoRecommenderApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/cleo/CleoRecommender/CleoRecommenderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cleo.CleoRecommender;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CleoRecommenderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
